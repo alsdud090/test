93660316f5d99d5ae2d8815cb52b454df806860c
USER <USER@INET-08>
2018-03-13 19:43:33 +0900

    ... ...

---
+++ school/src/school/school.java
@@ -0,0 +1,50 @@
+package school;
+
+public class school {
+	String Title;
+	String Principal;
+	String Viceprincipal;
+	int Year;
+	int ClassRoom;
+	int Student;
+	int Teacher;
+	int Building;
+	int Class;
+	
+	
+	void print(){
+		System.out.println();
+	}
+}
+class school2 {
+	String Title;
+	String Principal;
+	String Viceprincipal;
+	int Year;
+	int ClassRoom;
+	int Student;
+	int Teacher;
+	int Building;
+	int Class;
+	
+	
+	void print(){
+		System.out.println();
+	}
+}
+class school3 {
+	String Title;
+	String Principal;
+	String Viceprincipal;
+	int Year;
+	int ClassRoom;
+	int Student;
+	int Teacher;
+	int Building;
+	int Class;
+	
+	
+	void print(){
+		System.out.println();
+	}
+}
---
+++ school/src/school/schoolTest.java
@@ -0,0 +1,22 @@
+package school;
+
+public class schoolTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		school s;
+		s = new school();
+		s.ClassRoom = 12;
+		s.Student = 300;
+		s.Teacher = 20;
+		s.Year = 2000;
+		s.Building = 3;
+		s.Class = 3;
+		s.Principal = "ooo";
+		s.Title = "ooo middle School";
+		s.Viceprincipal = "ooo";
+		s.print();
+		
+	}
+
+}
